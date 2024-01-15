66dcb5c99bf8010424336ef8ae7378cc6d37d876
Akshay <akshay10.bhardwaj@gmail.com>
2024-01-15 18:04:23 +0000

    ADD: Encoder

--- model.py
+++ model.py
@@ -178,3 +178,15 @@ class EncoderBlock(nn.Module):
         )
         x = self.residual_connection_2(x, self.feed_forward_block)
         return x
+
+
+class Encoder(nn.Module):
+    def __init__(self, layers: nn.ModuleList) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
+
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
