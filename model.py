b2c2ab2001744029c3dbfc79ffe801dab1c9f311
Akshay <akshay10.bhardwaj@gmail.com>
2024-01-15 18:16:05 +0000

    ADD: Final linear layer (softmax)

--- model.py
+++ model.py
@@ -232,3 +232,13 @@ class Decoder(nn.Module):
         for layer in self.layers:
             x = layer(x, encoder_output, src_mask, tgt_mask)
         return self.norm(x)
+
+
+class ProjectionLayer(nn.Module):
+    def __init__(self, d_model: int, vocab_size: int) -> None:
+        super().__init__()
+        self.linear = nn.Linear(d_model, vocab_size)
+
+    def forward(self, x):
+        # (batch_size, seq_len, d_model) --> (batch_size, seq_len, vocab_size
+        return torch.log_softmax(self.linear(x), dim=-1)
