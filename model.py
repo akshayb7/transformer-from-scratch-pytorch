3ac8adcb153a51c234ad0ff2907b44764e2cde21
Akshay <akshay10.bhardwaj@gmail.com>
2024-01-16 15:33:03 +0000

    ADD: Transformer block as a whole

--- model.py
+++ model.py
@@ -242,3 +242,35 @@ class ProjectionLayer(nn.Module):
     def forward(self, x):
         # (batch_size, seq_len, d_model) --> (batch_size, seq_len, vocab_size
         return torch.log_softmax(self.linear(x), dim=-1)
+
+
+class Transformer(nn.Module):
+    def __init__(
+        self,
+        encoder: Encoder,
+        decoder: Decoder,
+        src_embed: InputEmbeddings,
+        tgt_embed: InputEmbeddings,
+        pos_embed: PositionalEncoding,
+        projection_layer: ProjectionLayer,
+    ) -> None:
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_embed = src_embed
+        self.tgt_embed = tgt_embed
+        self.pos_embed = pos_embed
+        self.projection_layer = projection_layer
+
+    def encode(self, src, src_mask):
+        src = self.src_embed(src)
+        src = self.pos_embed(src)
+        return self.encoder(src, src_mask)
+
+    def decode(self, tgt, encoder_output, src_mask, tgt_mask):
+        tgt = self.tgt_embed(tgt)
+        tgt = self.pos_embed(tgt)
+        return self.decoder(tgt, encoder_output, src_mask, tgt_mask)
+
+    def project(self, x):
+        return self.projection_layer(x)
